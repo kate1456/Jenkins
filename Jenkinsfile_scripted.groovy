f5de1f58de55c180c210356cd805155bff330acc
Kate Chadova <Kity>
2023-01-26 12:17:48 +0300

    настройки для дженкинса8

--- Jenkinsfile_scripted.groovy
+++ Jenkinsfile_scripted.groovy
@@ -4,7 +4,7 @@ node {
     stage('Checkout SCM') {
         checkout(
                 [$class: 'GitSCM',
-                 branches: [[name: "/main"]],
+                 branches: [[name: "refs/heads/${BRANCH}"]],
                  userRemoteConfigs: [[url: 'https://github.com/kate1456/Jenkins.git']]]
         )
     }
